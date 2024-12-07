4e60bfe1c7bf6399e34473506435e73351047b89
Emerson <emersonpedrosacontato@gmail.com>
2024-12-07 11:52:58 -0300

    Iniciando Projeto

---
+++ src/main/java/com/github/com/Emerson_P/Projeto/Spring/ProjetoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.github.com.Emerson_P.Projeto.Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/com/Emerson_P/Projeto/Spring/ProjetoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.com.Emerson_P.Projeto.Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
